fb47e28abca56f5b0e82d0e08e06e9c85d307747
maantano <maantano@naver.com>
2024-06-02 12:49:38 +0900

    list2()

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -102,6 +102,10 @@ export default function Header() {
   // useMemo(() => {
   //   console.log("loginState ====>", loginState.user);
   // }, [loginState]);
+
+  const list2 = () => {
+    console.log("list2 call");
+  };
   return (
     <header className="bg-white">
       <nav
